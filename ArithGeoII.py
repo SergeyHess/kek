46c73e4023a1920ec0c8fb97d7404b4cb51dae2d
SergeyHess <zaperistanks@gmail.com>
2020-05-12 03:17:09 +0300

    ку

--- ArithGeoII.py
+++ ArithGeoII.py
@@ -1,8 +1,14 @@
 def ArithGeoII(arr):
-    for i in range(1, len(arr)):
-        if not (arr[i] - arr[i-1] == arr[1] - arr[0]):
-            for i in range(1, len(arr)):
-                if not (arr[i]/arr[i-1] == arr[1]/arr[0]):
-                    return -1
-            return "Geometric"
-    return "Arithmetic"
+    if len(arr) > 2:
+        for i in range(1, len(arr)):
+            if not (arr[i] - arr[i-1] == arr[1] - arr[0]):
+                for i in range(1, len(arr)):
+                    if not (arr[i]/arr[i-1] == arr[1]/arr[0]):
+                        return -1
+                return "Geometric"
+        return "Arithmetic"
+    return -1
+
+arr_ = [2, 4, 8]
+
+print(ArithGeoII(arr_))
